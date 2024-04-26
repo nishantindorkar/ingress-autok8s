89bb1f5382db546b26d29b24ba9ad83f55fc337b
nishant <nishant@gmail.com>
2024-04-26 17:56:32 +0530

    groovy file test3

--- deploy.groovy
+++ deploy.groovy
@@ -13,13 +13,13 @@ pipeline {
         stage('Git Pull') {
             steps {
                 sh 'sudo apt update -y'
-                //git credentialsId: 'jenkins', url: "${REPO_URL}"
+                git credentialsId: 'ingress', url: "${REPO_URL}"
                 }
         }
         stage("Build Maven") {
             steps { 
-                sh 'sudo apt-get update -y'
-                sh 'sudo apt-get install maven curl unzip -y'
+                // sh 'sudo apt-get update -y'
+                // sh 'sudo apt-get install maven curl unzip -y'
                 sh 'mvn clean package'
             }
         }
